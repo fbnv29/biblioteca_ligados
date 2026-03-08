163b4a69ddfaf493cc91f1e3d90326bea5f7936f
fbnv29 <fbnv29@gmail.com>
2026-03-08 18:10:39 -0300

    Fix: Add PUT to CORS allowed methods in Worker

--- worker/index.js
+++ worker/index.js
@@ -6,7 +6,7 @@ export default {
     // --- CORS Headers ---
     const corsHeaders = {
       "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, DELETE, OPTIONS",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
       "Access-Control-Allow-Headers": "Content-Type, Authorization",
     };
 
